bc948a9a3754c9c3da070fa8bcba7338652b955b
David Expósito de Carvalho <69867572+davidexpcarvalho@users.noreply.github.com>
2023-10-13 18:25:20 -0300

    Update TwitchTrendsAnalyzer.py
    Selenium, como sempre.

--- TwitchTrendsAnalyzer.py
+++ TwitchTrendsAnalyzer.py
@@ -10,10 +10,11 @@ locale.setlocale(locale.LC_MONETARY, 'pt_BR.UTF-8')
 def fetch_twitch_data(url):
     # Adicione o path do seu webdriver se necessário
     from selenium.webdriver.common.by import By
-    from selenium import webdriver
+     from selenium import webdriver
+    from selenium.webdriver.chrome.service import Service as ChromeService
     from webdriver_manager.chrome import ChromeDriverManager
 
-    driver = webdriver.Chrome(executable_path=ChromeDriverManager().install())
+    driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install())
     driver.get(url)
     time.sleep(5)
     
